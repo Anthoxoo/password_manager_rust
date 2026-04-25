c019ecb1556dee396f9d02781c87638389b369d6
Anthoxoo <antoine.guenot31450@gmail.com>
2026-04-25 21:53:41 +0200

    library close to be finished, missing the encrypt function

--- src/lib.rs
+++ src/lib.rs
@@ -6,11 +6,12 @@ pub struct PasswordManager {
     password: HashMap<String, Password>,
 }
 
-struct Password {
+pub struct Password {
     username: String,
     password: String,
 }
 
+#[derive(Debug, PartialEq)]
 enum State {
     Locked,
     Unlocked,
@@ -25,31 +26,76 @@ impl PasswordManager {
         }
     }
 
-    pub fn open_manager(&mut self, master_pass: String) {
-        todo!()
+    pub fn open_manager(&mut self, master_pass: String) -> Result<(), &'static str> {
+        if master_pass == self.master_password {
+            self.state = State::Unlocked;
+            Ok(())
+        } else {
+            Err("Wrong password.")
+        }
     }
 
     pub fn close_manager(&mut self) {
-        todo!()
+        self.state = State::Locked
     }
 
-    pub fn add_password(&mut self, url: String, username: String, password: String) {
-        todo!()
+    pub fn add_password(
+        &mut self,
+        url: String,
+        username: String,
+        password: String,
+    ) -> Result<(), &'static str> {
+        if self.state == State::Locked {
+            Err("The manager is locked.")
+        } else {
+            let new_password = Password {
+                username: username,
+                password: encrypt_password(password),
+            };
+
+            self.password.insert(url, new_password);
+            Ok(())
+        }
     }
 
-    pub fn modify_password(&mut self, url: String, username: String) {
-        todo!()
+    pub fn modify_password(
+        &mut self,
+        url: String,
+        username: String,
+        new_password: String,
+    ) -> Result<(), &'static str> {
+        if self.state == State::Locked {
+            return Err("The manager is locked.");
+        }
+        if let Some(entry) = self.password.get_mut(&url) {
+            entry.username = username;
+            entry.password = new_password;
+
+            Ok(())
+        } else {
+            Err("Url not found.")
+        }
     }
 
-    pub fn delete_password(&mut self, url: String, username: String) {
-        todo!()
+    pub fn delete_password(&mut self, url: String) -> Result<(), &'static str> {
+        if self.state == State::Locked {
+            return Err("The manager is locked.");
+        }
+        if self.password.remove(&url).is_some() {
+            Ok(())
+        } else {
+            Err("Url not found.")
+        }
     }
 
     pub fn list_passwords(&self) -> Result<Vec<&Password>, &'static str> {
-        todo!()
+        if self.state == State::Locked {
+            return Err("The manager is locked.");
+        }
+        Ok(self.password.values().collect())
     }
+}
 
-    fn encrypt_password(password: String) -> String {
-        todo!()
-    }
+fn encrypt_password(password: String) -> String {
+    todo!()
 }
